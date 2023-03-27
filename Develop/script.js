8f6cd282a69bae9afe9dfd8c626a33c80e249c03
LilNewday <lilnewday@gmail.com>
2023-03-27 02:04:01 -0400

    Added more comments and a little more efficient

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,14 @@
 // Array for Special Characters
-var SpecialC= ['~','!','@','#','$','%','^','&','*','(',')','-','_','+','[',']','{','}','\\','|',';',':',',','<','.','>','?','/','`','=','"'];
+const SpecialC= ['~','!','@','#','$','%','^','&','*','(',')','-','_','+','[',']','{','}','\\','|',';',':',',','<','.','>','?','/','`','=','"'];
 
 // Array for Numeric Characters
-var NumericC= ['0','1','2','3','4','5','6','7','8','9'];
+const NumericC= ['0','1','2','3','4','5','6','7','8','9'];
 
 // Array for Lower Case Characters
-var LowerCaseC= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const LowerCaseC= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 // Array for Upper Case Characters
-var UpperCaseC= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const UpperCaseC= ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 // Prompt for Password Requirements
 function PasswordCriteria() {
@@ -17,29 +17,24 @@ function PasswordCriteria() {
     10
   );
   // Has to at least be a number
-  if (Number.isNaN(length)) {
+  if (!Number.isInteger(length)) {
     alert('Password length requires a number');
     return null;
   }
-  // Less than 128 Characters, Fails otherwise
-  if (length > 128) {
-    alert('Password must be less than 129 characters');
-    return null;
-  }
-  // At least 8 Characters, Fails otherwise
-  if (length < 8) {
-    alert('Password must be more than 7 characters');
+  // At least 8 characters and less than 128 Characters, Fails otherwise
+  if (length < 8 || length > 128) {
+    alert('Password must be more than 7 characters and less than 129 characters');
     return null;
   }
 
 // Confirm inclusion of Special Characters
-  var confirmSpecial = confirm('Click OK to confirm special characters');
+  const confirmSpecial = confirm('Click OK to confirm special characters');
 // Confirm inclusion of Numeric Characters
-  var confirmNumbers = confirm('Click OK to confirm numeric characters');
+  const confirmNumbers = confirm('Click OK to confirm numeric characters');
 // Confirm inclusion of Lowercased Characters
-  var confirmLowercased = confirm('Click OK to confirm lowercased characters');
+  const confirmLowercased = confirm('Click OK to confirm lowercased characters');
 // Confirm inclusion of Uppercased Characters
-  var confirmUppercased = confirm('Click OK to confirm uppercased characters');
+  const confirmUppercased = confirm('Click OK to confirm uppercased characters');
 
 // Checks to make sure at least one Character was selected
   if (
@@ -62,18 +57,20 @@ function PasswordCriteria() {
 
   return Choice;
 }
-
+// Is the random function that allows the password to be random
 function Random(array) {
   var RandomIndex = Math.floor(Math.random() * array.length);
   var RandomElement = array[RandomIndex];
 
   return RandomElement;
 }
-
+// Allows the Password to generate
 function generatePassword() {
   var options = PasswordCriteria();
   var result = [];
+  // Stores the potential types of characters used
   var possiblecharacters = [];
+  // Are the guaranteed characters being used
   var guaranteedcharacters = [];
     
   if (!options) return null;
